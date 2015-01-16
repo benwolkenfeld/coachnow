1017a36101138499fa4b054d95ec69f1d0f53cfd
benwolkenfeld <bwolkenfeld@yahoo.com>
2015-01-16 14:41:37 -0600

    reorganized app structure

--- app/shared/scripts/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('coachNow', [
-  'ui.bootstrap',
-  'coachNow.actionservice',
-  'coachNow.actioncontroller'
-  ]);
-
-//var homeUrl = "http://10.37.129.2/~benwolkenfeld/CoachNow/";  // for testing in IE on parallels
-var homeUrl = "http://localhost/~benwolkenfeld/CoachNow/";
-//var homeUrl = 'http://benwolkenfeld.github.io/coachnow/';
